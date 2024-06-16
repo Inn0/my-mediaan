998f6113eef6c0803278b08b998d5e99f7cd34a3
Ivans Mihailovs <ivans.mihailovs@mediaan.com>
2024-06-16 12:59:15 +0200

    feat: Create MyMediaanApp file

--- app/src/main/java/com/mediaan/mymediaan/MyMediaanApp.kt
+++ app/src/main/java/com/mediaan/mymediaan/MyMediaanApp.kt
@@ -1,9 +1,5 @@
 package com.mediaan.mymediaan
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,22 +9,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mediaan.mymediaan.ui.theme.MyMediaanTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MyMediaanTheme {
-                MyMediaanApp()
-            }
-        }
+@Composable
+fun MyMediaanApp() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
     }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MyMediaanAppPreview() {
+fun GreetingPreview() {
     MyMediaanTheme {
-        MyMediaanApp()
+        Greeting("Android")
     }
 }
