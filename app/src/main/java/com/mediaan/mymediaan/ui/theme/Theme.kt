01fc6bf68e75869b9224e6326e507e0a5108de14
Inn0 <daan.brocatus@outlook.com>
2024-06-18 12:50:51 +0200

    refactor: remove unused import
    Refs: #12

--- app/src/main/java/com/mediaan/mymediaan/ui/theme/Theme.kt
+++ app/src/main/java/com/mediaan/mymediaan/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.mediaan.mymediaan.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
